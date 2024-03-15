1f4ea5dd454f66ffb6e84804d44730f240bb2680
TheMatiaz0 <TheMatiaz0@protonmail.com>
2024-03-15 15:06:41 +0100

    Add primitive item creation process (based on instantiate)

--- Assets/Scripts/Inventory/InventoryView.cs
+++ Assets/Scripts/Inventory/InventoryView.cs
@@ -1,18 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using TNRD;
 using UnityEngine;
 
-public class InventoryView : MonoBehaviour
+public class InventoryView : MonoBehaviour, IActivable
 {
     [SerializeField]
     private InventorySlot slotPrefab;
 
     [SerializeField]
-    private IInventoryReader inventoryReader;
+    private Transform slotParent;
 
-    private void Awake()
+    [SerializeField]
+    private SerializableInterface<IInventoryReader> inventoryReader;
+
+    [SerializeField]
+    private SerializableInterface<IActivable> view;
+
+#if UNITY_EDITOR
+
+    [Header("Editor Debug")]
+    [SerializeField]
+    private SerializableInterface<IInventoryReader> editorInventoryReader;
+
+#endif
+
+    public bool IsActive => view.Value.IsActive;
+
+    public void Activate()
     {
-        // get data from InventoryDefinition
+        view.Value?.Activate();
+        CreateItems(inventoryReader.Value);
     }
+
+    private void CreateItems(IInventoryReader inventoryReader)
+    {
+        if (inventoryReader == null)
+        {
+            return;
+        }
+
+        foreach (Transform child in slotParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (var itemDefinition in inventoryReader.Items)
+        {
+            var inventorySlot = Instantiate(slotPrefab, slotParent);
+            inventorySlot.Refresh(itemDefinition);
+        }
+    }
+
+    public void Deactivate()
+    {
+        view.Value?.Deactivate();
+    }
+
+#if UNITY_EDITOR
+
+    [ContextMenu("Refresh Editor Items")]
+    private void OnValidate()
+    {
+        CreateItems(editorInventoryReader.Value);
+    }
+
+#endif
 }
